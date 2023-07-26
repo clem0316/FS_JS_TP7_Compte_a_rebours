2dfa96851e7f9210210dcea923161bdffa23b98e
Clément Badin <clement@macbook-air-de-clement.home>
2023-07-26 19:37:24 +0200

    Compte à rebours réussi avec amélioration et commentaire

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-// Créer la logique de compte à rebours
-
-// Créer un événement à la validation du form pour lancer le compte à rebours
+let zero = "";
+// J'ai créé cette fonction pour ajouter un zéro devant les secondes inférieures à 10
+let totalSeconds;
+let seconds;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let zero = "";
-  let totalSeconds = choice.value * 60;
-  let seconds = totalSeconds % 60;
+  totalSeconds = choice.value * 60;
+  seconds = totalSeconds % 60;
+  // Ci-dessus on récupère le reste des secondes qui ne constituent pas une minute pleine
 
   let minutes = Math.floor(totalSeconds / 60);
   setInterval(() => {
-    if (minutes === 0 && seconds === 0) return;
+    if (minutes === 0 && seconds === 0) {
+      countdownDisplay.innerHTML = `C'est terminé !`;
+      return;
+    }
     if (seconds === 0 && minutes > 0) {
       minutes--;
       seconds = 60;
