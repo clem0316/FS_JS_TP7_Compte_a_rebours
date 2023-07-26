f08a56a2e444ae0801beab1a7a9f255ee0ff15a2
Clément Badin <clement@macbook-air-de-clement.home>
2023-07-26 19:29:26 +0200

    compte à rebours réussi ! avant correction

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 // Créer la logique de compte à rebours
 
-choice.value = 5 minutes
-
-choice.value * 60 = totalSeconds
-
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = totalSeconds % 60
-
 // Créer un événement à la validation du form pour lancer le compte à rebours
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let zero = "";
+  let totalSeconds = choice.value * 60;
+  let seconds = totalSeconds % 60;
+
+  let minutes = Math.floor(totalSeconds / 60);
+  setInterval(() => {
+    if (minutes === 0 && seconds === 0) return;
+    if (seconds === 0 && minutes > 0) {
+      minutes--;
+      seconds = 60;
+      zero = "";
+    }
+    if (seconds > 0) {
+      seconds--;
+    }
+    if (seconds < 10) {
+      zero = "0";
+    }
+    countdownDisplay.innerHTML = `${minutes}:${zero}${seconds}`;
+  }, 1000);
+});
